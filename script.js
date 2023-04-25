2f14f776060a5ef18b90ecb67b0e77d498aaeab2
Artem8745 <102891549+Artem8745@users.noreply.github.com>
2023-04-25 20:27:01 +0400

    add

---
+++ script.js
@@ -0,0 +1,41 @@
+// Прокрутка при клике 
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target
+        // Проверка ошибок
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto)
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight // Учитывает расстояние шапки
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            })
+            e.preventDefault()
+        }
+    }
+}
+
+window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+  
+    if (window.innerWidth > 768) {
+      document.querySelectorAll('.section').forEach((el, i) => {
+        if (el.offsetTop - document.querySelector('.navbar').clientHeight <= scrollDistance) {
+          document.querySelectorAll('.navbar a').forEach((el) => {
+            if (el.classList.contains('active')) {
+              el.classList.remove('active');
+            }
+          });
+  
+          document.querySelectorAll('.navbar li')[i].querySelector('a').classList.add('active');
+        }
+      });
+    }
+  });
